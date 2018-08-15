90e5de472d878a9ff95ececa7eeb9698c6b2fdd8
Andrew Cosmic <acdickin@gmail.com>
2018-08-15 14:23:29 -0700

    Each color has extra info

--- src/view/ColorInfo.js
+++ src/view/ColorInfo.js
@@ -1,12 +1,27 @@
 import React from 'react';
 import {Text, View, StyleSheet} from 'react-native'
+import Color from 'color'
 
-const ColorInfo =({navigation})=>(
-  <View style={styles.container}>
-    <Text style={styles.txt}>{navigation.state.params.color} </Text>
-  </View>
-)
-ColorInfo.navigationOptions=({navigation})=>({ 
+const ColorInfo =({navigation})=>{
+  const color = Color(navigation.state.params.color);
+  return(
+    <View style={[styles.container, {backgroundColor: color}]}>
+      <Text style={[styles.txt, {color:color.negate()}]}>
+        {color.hex()}
+      </Text>
+      <Text style={[styles.txt, {color:color.negate()}]}>
+        {color.rgb().string()}
+      </Text>
+      <Text style={[styles.txt, {color:color.negate()}]}>
+        {color.hsl().string()}
+      </Text>
+      <Text style={[styles.txt, {color:color.negate()}]}>
+        Text color:{color.negate().hex()}
+      </Text>
+    </View>
+  )
+}
+ColorInfo.navigationOptions=({navigation})=>({
   title:navigation.state.params.color
 })
 export default ColorInfo
