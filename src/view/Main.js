ad133d84bb13eaf3930fb0fafe0f4356af886c0b
Andrew Cosmic <acdickin@gmail.com>
2018-08-15 14:43:21 -0700

    fixed deprecated stack nav

--- src/view/Main.js
+++ src/view/Main.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet,Alert  } from 'react-native';
+import { StyleSheet } from 'react-native';
 import ColorList from './ColorList'
 import ColorInfo from './ColorInfo'
 import {createStackNavigator} from 'react-navigation'
